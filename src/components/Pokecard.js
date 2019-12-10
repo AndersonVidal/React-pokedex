b01ca21cfdee94dd3d10109b017994b5025c301c
AndersonVidal <anderson.vidal@ccc.ufcg.edu.br>
2019-12-10 18:51:02 -0300

    ajusts

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -4,20 +4,18 @@ import '../styles/pokecard.css'
 const pokeUrl = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
 
 class Pokecard extends Component {
-  makeimgId(id) {
-    let imgStr = ''
-    if (id < 100) imgStr = (id < 10) ? '00' : '0'
-    return imgStr + id + '.png'
+  makeImgURL(id) {
+    let imgStr = (id <= 999) ? `00${id}`.slice(-3) : id
+    return `${pokeUrl}${imgStr}.png`
   }
 
   render() {
     return (
       <div className='Pokecard'>
-        <h3 className='Poketitle'>{this.props.name}</h3>
-        <img className='Pokeimg' src={pokeUrl + this.makeimgId(this.props.id)} alt="" />
-        <div>
-          <p>Type: {this.props.type} <br/> EXP: {this.props.exp} </p>
-        </div>
+        <h2 className='Poketitle'>{this.props.name}</h2>
+        <img className='Pokeimg' src={this.makeImgURL(this.props.id)} alt={this.props.name} />
+        <div className='Pokedata'>Type: {this.props.type}</div>
+        <div className='Pokedata'>EXP: {this.props.exp}</div>
       </div>
     )
   }
